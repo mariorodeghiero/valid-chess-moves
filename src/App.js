a1dcac71b929d27579f40081dc3f6b2becf94206
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-01-13 20:59:36 -0200

    add: knight.js, chessboard.js, styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => <h1>Initialized project</h1>;
+import Chessboard from './Components/Chessboard';
+/**
+ * TODO:
+ *  [] -  sendPosition
+ *  [] -  selectedCell
+ *  [] -  only one cell can be selected
+ *  [] -  highlighted
+ *  [] -  knighPosition
+ *  [] -  possiblePositions
+ *  [] -  algebraicNotation
+ * FIXME:
+ *  [] -
+ */
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // highlighted: [],
+      // selectedCell: [],
+      // algebraicNotation: '',
+      // knighPosition: [],
+      // sendPosition: false,
+      // possiblePositions: [],
+      // moved: false,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Challenge: Valid Chess Moves</h1>
+        <Chessboard />
+      </div>
+    );
+  }
+}
