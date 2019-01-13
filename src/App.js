f2ed26275c59f54498468a4c0ed370a200d7b177
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-01-12 23:37:14 -0200

    created branch develop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const App = () => <h1>Project initiated</h1>;
+const App = () => <h1>Initialized project</h1>;
 
 export default App;
