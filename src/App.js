a65345631a38882ac4ac902aed283e527c66d95e
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-01-12 23:29:34 -0200

    initialized project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const App = () => <h1>Started project</h1>;
+const App = () => <h1>Project initiated</h1>;
 
 export default App;
